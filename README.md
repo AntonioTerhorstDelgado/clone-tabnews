728be5fab8401e9108614638570724e99eaa3dce
Antônio Terhorst <84198804+AntonioTerhorstDelgado@users.noreply.github.com>
2024-12-30 12:46:44 +0000

    adiciona scripts `lint:check` e `lint:flix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Este é um site do meu curso do filipedeschamps, curso.dev, aqui estará tudo o que eu aprendi neste curso
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 function Home() {
-
-  return <h1>primeira frase da minha pagina incial! 😁</h1>
+  return <h1>primeira frase da minha pagina incial! 😁</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
