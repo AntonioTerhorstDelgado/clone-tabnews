973de89b89b73a61993cef82f19217a9f473490f
Antônio Terhorst <84198804+AntonioTerhorstDelgado@users.noreply.github.com>
2024-12-30 12:48:13 +0000

    adiciona scripts `lint:check` e `lint:flix`

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,4 @@ function Home() {
   return <h1>primeira frase da minha pagina incial! 😁</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
