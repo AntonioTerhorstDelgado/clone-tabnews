f31d6fa2b9671752c83650669e7b3c7ccd454c7f
Antônio Terhorst <84198804+AntonioTerhorstDelgado@users.noreply.github.com>
2025-01-08 16:37:24 +0000

    makes `database.js` `client.connect()` more robust to errors

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -2,12 +2,12 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
-  const responsedBody = await response.json();
+  const responseBody = await response.json();
 
-  const parsedUpdateAt = new Date(responsedBody.updaded_at).toISOString();
-  expect(responsedBody.updaded_at).toEqual(parsedUpdateAt);
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responsedBody.dependecies.database.version).toEqual("16.0");
-  expect(responsedBody.dependecies.database.max_connections).toEqual(100);
-  expect(responsedBody.dependecies.database.opened_connection).toEqual(1);
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  expect(responseBody.dependecies.database.opened_connection).toEqual(1);
 });
