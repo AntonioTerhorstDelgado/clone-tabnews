e74dd585506923d498dd4507ac34f771e4e5d1cc
Antônio Terhorst <84198804+AntonioTerhorstDelgado@users.noreply.github.com>
2025-01-09 17:34:27 +0000

    Formatting my code

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,10 +3,8 @@ import { version } from "react";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
-
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
@@ -17,10 +15,8 @@ async function status(request, response) {
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
-
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
